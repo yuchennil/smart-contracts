e59ed1b9485bb9117fa318f07ffd989226a957ba
Harold Hyatt <haroldhyatt@Hals-MBP.hsd1.ca.comcast.net>
2020-08-04 14:47:14 -0700

    saft script

--- scripts/register_saft_addresses.ts
+++ scripts/register_saft_addresses.ts
@@ -11,14 +11,14 @@ import { TimeLockRegistryFactory } from '../build/types/TimeLockRegistryFactory'
 import { TrustToken } from '../build/types/TrustToken'
 import { TrustTokenFactory } from '../build/types/TrustTokenFactory'
 
-export const txnArgs = { gasLimit: 2_000_000, gasPrice: 20_000_000_000 }
+export const txnArgs = { gasLimit: 100_000, gasPrice: 40_000_000_000 }
 
 export interface SaftAccount {
   address: string,
   amount: string,
 }
 
-const toTrustToken = (amount: string) => utils.parseUnits(amount, 8)
+const toTrustToken = (amount: string) => utils.parseUnits(amount, 6)
 const sum = (numbers: utils.BigNumber[]) => numbers.reduce((cumSum, value) => cumSum.add(value), constants.Zero)
 
 export const registerSaftAccounts = async (registry: TimeLockRegistry, trustToken: TrustToken, accounts: SaftAccount[]) => {
@@ -29,8 +29,9 @@ export const registerSaftAccounts = async (registry: TimeLockRegistry, trustToke
   let { nonce } = tx
   const pendingTransactions = []
   for (const { address, amount } of accounts) {
+    let decimalAmount = amount.slice(0, -2) + "." + amount.slice(-2)
     pendingTransactions.push((await registry.register(address, toTrustToken(amount), { ...txnArgs, nonce: nonce + 1 })).wait()
-      .then(() => console.log(`Done: ${address} for ${amount} TRU`))
+      .then(() => console.log(`Done: ${address} for ${decimalAmount} TRU`))
       .catch((err) => console.error(`Failed for ${address}`, err)),
     )
     nonce++
@@ -51,10 +52,15 @@ const readAccountList = (filePath: string) => parseAccountList(fs.readFileSync(f
 
 if (require.main === module) {
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const deployedAddresses = require(`./deploy/${process.argv[2]}.json`)
-  const provider = new providers.InfuraProvider(process.argv[2], '81447a33c1cd4eb09efb1e8c388fb28e')
+  const deployedAddresses = {
+    timeLockRegistry: "0x5Fe2F5F2Cc97887746C5cB44386A94061F35DcC4",
+    trustToken: "0x4C19596f5aAfF459fA38B0f7eD92F11AE6543784"
+  }
+
+  const provider = new providers.InfuraProvider(process.argv[2], 'e33335b99d78415b82f8b9bc5fdc44c0')
   const wallet = new Wallet(process.env.PRIVATE_KEY, provider)
   const registry = TimeLockRegistryFactory.connect(deployedAddresses.timeLockRegistry, wallet)
   const trustToken = TrustTokenFactory.connect(deployedAddresses.trustToken, wallet)
-  registerSaftAccounts(registry, trustToken, readAccountList(process.argv[3])).then(() => console.log('Done.'))
+  const accountList = readAccountList(process.argv[3])
+  registerSaftAccounts(registry, trustToken, accountList).then(() => console.log('Done.'))
 }
